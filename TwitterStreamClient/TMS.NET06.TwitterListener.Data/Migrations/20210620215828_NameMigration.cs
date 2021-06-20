3cd93614d68a7e42cd56c0389616b6ba5d759be3
silakovvv <75270532+silakovvv@users.noreply.github.com>
2021-06-21 01:10:45 +0300

    publish react project

---
+++ TwitterStreamClient/TMS.NET06.TwitterListener.Data/Migrations/20210620215828_NameMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TMS.NET06.TwitterListener.Data.Migrations
+{
+    public partial class NameMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
